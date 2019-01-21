66116fc0eab20d5e8276ecc60570623ef8e833fc
L.Frizzell <lf292@kent.ac.uk>
2019-01-21 14:16:54 +0000

    change switch statement to neater and more efficient array of hex.

--- 7-segment-display/counter.ino
+++ 7-segment-display/counter.ino
@@ -15,7 +15,7 @@ void loop()
 {
   // loop to turn leds od seven seg ON
   
-  for(int i=0;i<16;i++)
+  for(int i=0;i<=16;i++)
   {
     setDigit(i);
     delay(1000);
@@ -24,66 +24,18 @@ void loop()
 
 void setDigit(int digit)
 {
-  uint8_t segs;
 
-  switch(digit){
-    case 0:
-      segs = 0x3F;
-      break;
-    case 1:
-      segs = 0x06;
-      break;
-    case 2:
-      segs = 0x6B;
-      break;
-    case 3:
-      segs = 0x4F;
-      break;
-    case 4: 
-      segs = 0x56;
-      break;
-    case 5:
-      segs = 0x5D;
-      break;
-    case 6:
-      segs = 0x7D;
-      break;
-    case 7:
-      segs = 0x07;
-      break;
-    case 8:
-      segs = 0x7F;
-      break;
-    case 9:
-      segs = 0x5F;
-      break;
-    case 10:
-      segs = 0x77;
-      break;
-    case 11:
-      segs = 0x7C;
-      break;
-    case 12:
-      segs = 0x39;
-      break;
-    case 13:
-      segs = 0x6E;
-      break;
-    case 14:
-      segs = 0x79;
-      break;
-    case 15:
-      segs = 0x71;
-      break;
-    default:
-      segs = 0x00;
-      break;
+  if (digit < 0 || digit > 15){
+    clear();
+    return;
   }
+
+  uint8_t segs[] = {0x3F, 0x06, 0x6B, 0x4F, 0x56, 0x5D, 0x7D, 0x07, 0x7F, 0x5F, 0x77, 0x7C, 0x39, 0x6E, 0x79, 0x71};
   
   clear();
   
   for(int i = 0; i < 7; i++){
-    if((segs >> i) & 0x01) { 
+    if((segs[digit] >> i) & 0x01) { 
       digitalWrite(i+2, HIGH); 
     }
   }
